27967a3cd87f1539a1b34df6fa3796e9fcb5f772
lethekhoi <lethekhoi@gmail.com>
2020-06-14 21:46:27 +0700

    login page ui

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  double _deviceHeight;
+  double _deviceWidth;
+
+  GlobalKey<FormState> _formKey;
+
+  _LoginPageState() {
+    _formKey = GlobalKey<FormState>();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: Align(
+        alignment: Alignment.center,
+        child: _loginPageUI(),
+      ),
+    );
+  }
+
+  Widget _loginPageUI() {
+    return Container(
+      height: _deviceHeight * 0.7,
+      padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.1),
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          _headingWidget(),
+          _inputForm(),
+          _loginButton(),
+          _registerButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget _headingWidget() {
+    return Container(
+      height: _deviceHeight * 0.12,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Text(
+            "Welcome back!",
+            style: TextStyle(
+                color: Colors.white, fontSize: 30, fontWeight: FontWeight.w700),
+          ),
+          Text(
+            "Please login to your account",
+            style: TextStyle(
+                color: Colors.white, fontSize: 20, fontWeight: FontWeight.w200),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _inputForm() {
+    return Container(
+      height: _deviceHeight * 0.16,
+      child: Form(
+        key: _formKey,
+        onChanged: () {},
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            _emailTextField(),
+            _passwordTextField(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _emailTextField() {
+    return TextFormField(
+      autocorrect: false,
+      style: TextStyle(color: Colors.white),
+      validator: (_input) {},
+      onSaved: (_input) {},
+      cursorColor: Colors.white,
+      decoration: InputDecoration(
+        hintText: "Email Address",
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget _passwordTextField() {
+    return TextFormField(
+      obscureText: true,
+      autocorrect: false,
+      style: TextStyle(color: Colors.white),
+      validator: (_input) {},
+      onSaved: (_input) {},
+      cursorColor: Colors.white,
+      decoration: InputDecoration(
+        hintText: "Password",
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget _loginButton() {
+    return Container(
+      height: _deviceHeight * 0.08,
+      width: _deviceWidth,
+      child: MaterialButton(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25.0),
+            side: BorderSide(color: Colors.blue)),
+        onPressed: () {},
+        color: Colors.blue,
+        child: Text(
+          "LOGIN",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 15,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _registerButton() {
+    return GestureDetector(
+      onTap: () {
+        print("Hey");
+      },
+      child: Container(
+        height: _deviceHeight * 0.08,
+        width: _deviceWidth,
+        child: Text(
+          "REGISTER",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 15,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
+    );
+  }
+}
